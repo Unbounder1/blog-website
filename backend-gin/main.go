dd00a413ece30a140d7bd6bb2cf45b0848508103
Ryan Dong <78339287+Unbounder1@users.noreply.github.com>
2024-12-25 21:03:29 -0800

    added backend directory, learning gin syntax,

---
+++ backend-gin/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func LoggerMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		start := time.Now()
+		c.Next()
+		duration := time.Since(start)
+		log.Printf("Request - Method: %s | Status: %d | Duration: %v", c.Request.Method, c.Writer.Status(), duration)
+	}
+}
+
+func main() {
+	router := gin.Default()
+
+	router.Use(LoggerMiddleware())
+
+	router.GET("/", func(c *gin.Context) {
+		c.String(200, "Hello, World!")
+	})
+
+	router.GET("/bye", func(c *gin.Context) {
+		c.String(200, "Goodbye, World!")
+	})
+
+	router.Run(":8080")
+}
