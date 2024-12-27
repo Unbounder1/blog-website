c58cd4658d5a3d0ce20f87437a780ef630a2e5a3
Ryan Dong <78339287+Unbounder1@users.noreply.github.com>
2024-12-26 19:59:36 -0800

    added supabase test queries

--- backend-gin/main.go
+++ backend-gin/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"log"
+	"net/http"
+	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-resty/resty/v2"
 )
 
 func LoggerMiddleware() gin.HandlerFunc {
@@ -17,6 +20,10 @@ func LoggerMiddleware() gin.HandlerFunc {
 }
 
 func main() {
+
+	secretKey := os.Getenv("SECRET_KEY")
+	projectUrl := os.Getenv("PROJECT_URL")
+
 	router := gin.Default()
 
 	router.Use(LoggerMiddleware())
@@ -27,6 +34,21 @@ func main() {
 
 	router.GET("/bye", func(c *gin.Context) {
 		c.String(200, "Goodbye, World!")
+	})
+
+	router.GET("/users", func(c *gin.Context) {
+		client := resty.New()
+		resp, err := client.R().
+			SetHeader("apikey", secretKey).
+			SetHeader("Authorization", "Bearer "+secretKey).
+			Get(projectUrl + "/rest/v1/users")
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"data": resp.String()})
 	})
 
 	router.Run(":8080")
