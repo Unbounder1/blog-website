7d9fb0b9314f2b61ca29236b82520fcdc282c983
Ryan Dong <78339287+Unbounder1@users.noreply.github.com>
2024-12-29 21:12:47 -0800

    Added tag to blogDigest columns

--- backend-gin/internal/interface/digest.go
+++ backend-gin/internal/interface/digest.go
@@ -14,6 +14,7 @@ type blogDigest struct {
 	Thumbnail_url string `json:"thumbnail_url"`
 	Created_at    string `json:"created_at"`
 	Updated_at    string `json:"updated_at"`
+	Tags          string `json:"tags"`
 }
 
 // TODO: Implement sharded querying (10 at a time ie)
@@ -27,7 +28,7 @@ func GetBlogs(db *sql.DB) ([]blogDigest, error) {
 	defer rows.Close()
 	for rows.Next() {
 		var dig blogDigest
-		err := rows.Scan(&dig.Title, &dig.Summary, &dig.Thumbnail_url, &dig.Created_at, &dig.Updated_at)
+		err := rows.Scan(&dig.Title, &dig.Summary, &dig.Thumbnail_url, &dig.Created_at, &dig.Updated_at, &dig.Tags)
 		if err != nil {
 			log.Fatalf("Error scanning row: %v", err)
 		}
