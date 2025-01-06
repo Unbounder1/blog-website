2c1f7beb760dd3d21ff04497729041c9828cb920
Ryan Dong <78339287+Unbounder1@users.noreply.github.com>
2025-01-06 14:54:45 -0500

    Added chapter and subchapter handling, removed declaration based yaml of chapters

--- admin-panel/blog/compile.py
+++ admin-panel/blog/compile.py
@@ -5,9 +5,8 @@ import markdown
 import yaml
 from dotmap import DotMap
 
-'''
-jinja filter function in control of image formatting. 
-'''
+toc_dict = [{}]
+
 def image_process(image_link, alt_text="", image_size="med"):
     if (image_size == "large"):
         width = ""
@@ -21,16 +20,78 @@ def image_process(image_link, alt_text="", image_size="med"):
     outStr = '<img\nsrc="' + image_link + '"\nalt="' + alt_text + '"\nwidth="' + width + '"\nheight="' + height + '"\n/>'
     return outStr
 
-def convert_html(dir, metadata):
+def chapter(id, title):
+    temp_dict = {}
+    temp_dict["number"] = id
+    temp_dict["title"] = title
+    temp_dict["subchapters"] = []
+    toc_dict.append(temp_dict)
+    return f'        <section id="{id}"></section>\n            <h2>{id}. {title}</h2>'
+
+def subchapter(id, title):
+    temp_dict = {}
+    temp_dict["number"] = id
+    temp_dict["title"] = title
+    toc_dict[-1]["subchapters"].append(temp_dict)
+    return f'        <section id="{id}"></section>\n            <h3>{id}. {title}</h3>'
+
+def html_convert(dir, metadata):
     f = open("./" + dir + "/body.md")
     html_content = markdown.markdown(f.read())
+    f.close()
 
     env = Environment()
     env.filters["image_process"] = image_process
+    env.filters["chapter"] = chapter
+    env.filters["subchapter"] = subchapter
+
 
     template = env.from_string(html_content)
 
     return template.render(metadata=metadata)
+
+def toc_process(toc):
+    """
+    Returns:
+    str: A string containing the HTML of the Table of Contents.
+    """
+
+    output = """
+    <div class="toc">
+        <h2>Table of Contents</h2>
+        <ul>
+    """
+
+    # Iterate through each chapter in the toc
+    for chapter in toc:
+        chapter_number = chapter.get('number')
+        chapter_title = chapter.get('title')
+        subchapters = chapter.get('subchapters', [])
+
+        output += f'            <li><a href="#{chapter_number}">{chapter_number}. {chapter_title}</a>\n'
+
+        # If there are subchapters, create a nested list
+        if subchapters:
+            output += '                <ul>\n'
+            for subchapter in subchapters:
+                sub_number = subchapter.get('number')
+                sub_title = subchapter.get('title')
+
+                sub_id = f"chapter{chapter_number}-sub{sub_number}".replace(" ", "-").lower()
+
+                output += f'                    <li><a href="#{sub_number}">{sub_number}. {sub_title}</a></li>\n'
+            output += '                </ul>\n'
+
+        # Close the chapter list item
+        output += '            </li>\n'
+
+    # Close the main list and container div
+    output += """
+        </ul>
+    </div>
+    """
+
+    return output
 
 def main():
     # code struct:
@@ -48,7 +109,22 @@ def main():
         metadata_dict = yaml.safe_load(file)
     metadata = DotMap(metadata_dict)
 
-    outputStr = convert_html(dir, metadata)
+    outputStr = ""
+    
+    outputStr += html_convert(dir, metadata)
+    outputStr = toc_process(toc_dict) + outputStr
+
+    #formatting
+    outputStr =  """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Understanding Python Virtual Environments</title>
+</head>
+<body>""" + outputStr
+    outputStr += "\n</body>\n</html>"
     print(outputStr)
 
     
