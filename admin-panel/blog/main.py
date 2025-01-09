ed667b0b1db752f807060f61a10ff3fa06e70173
Ryan Dong <78339287+Unbounder1@users.noreply.github.com>
2025-01-09 15:45:44 -0500

    implemented test single upload to minio -> works

--- admin-panel/blog/main.py
+++ admin-panel/blog/main.py
@@ -2,10 +2,12 @@ import os
 import sys
 sys.path.append(os.path.abspath('./.lib'))
 from compile import compile_dir, db_connection, delete_db
+from image_db import process_assets
 
 def main():
-    dirList = [d for d in os.listdir('.') if (os.path.isdir(d) and d[0] != "_" and d[0] != ".")]
-    print(dirList)
-    compile_dir("template")
+    # dirList = [d for d in os.listdir('.') if (os.path.isdir(d) and d[0] != "_" and d[0] != ".")]
+    # print(dirList)
+    # compile_dir("template")
+    process_assets("template")
 
 main()
