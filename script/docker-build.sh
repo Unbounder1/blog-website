96d4417aa27559a3001b93039c21c6433662f002
Ryan Dong <78339287+Unbounder1@users.noreply.github.com>
2025-01-13 19:54:58 -0500

    clean up directory

---
+++ script/docker-build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ $1 == "minikube" ] 
+then
+    echo "test"
+fi
+
+# eval $(minikube docker-env)
+
+# docker build ../backend-gin/deployments/postgres_db/Dockerfile
+
+# ../frontend/Dockerfile
+
+# ../backend-gin/deployments/postgres_db/Dockerfile
