ddeffd98da7b3ad66dca7539a464e787a44dd2c8
Ryan Dong <78339287+Unbounder1@users.noreply.github.com>
2025-02-05 00:44:35 -0500

    add resizeable functionality to terminal

--- frontend/src/components/full-set/BaseTerminal.jsx
+++ frontend/src/components/full-set/BaseTerminal.jsx
@@ -1,47 +1,45 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useEffect } from "react";
 import Draggable from "react-draggable";
+import { ResizableBox } from "react-resizable";
 import "../../styles/full-site/baseterminal.css";
 
 const DraggableTerminal = () => {
   const nodeRef = useRef(null);
-  const [position, setPosition] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
     console.log("Component mounted and hydrated.");
   }, []);
 
-  const handleDrag = (e, data) => {
-    console.log("Dragging:", data);
-    setPosition({ x: data.x, y: data.y });
-  };
-
   return (
-    <Draggable
-      nodeRef={nodeRef}
-      onStart={(e, data) => console.log("Drag started", e, data)}
-      onDrag={(e, data) => {
-        console.log("Dragging", e, data);
-        handleDrag(e, data);
-      }}
-      onStop={(e, data) => console.log("Drag stopped", e, data)}
-    >
-      <div ref={nodeRef} className="terminal">
-        {/* Tabs */}
-        <div className="tabs">
-          <div className="tab active">SRE</div>
-          <div className="tab">SWE</div>
-          <div className="tab">Kubernetes</div>
-          <div className="tab add-tab">+</div>
-        </div>
+    <ResizableBox
+        width={200}
+        height={200}
+        minConstraints={[100, 100]}
+        maxConstraints={[500, 500]}
+        axis="both"
+      >
+        <Draggable nodeRef={nodeRef} handle=".tabs">
+      
+        <div ref={nodeRef} className="terminal">
+          {/* Tabs */}
+          <div className="tabs">
+            <div className="tab active">SRE</div>
+            <div className="tab">SWE</div>
+            <div className="tab">Kubernetes</div>
+            <div className="tab add-tab">+</div>
+          </div>
 
-        {/* Terminal Body */}
-        <div className="terminal-body">
-          <div className="prompt">
-            ~/ryan-dong/site: <span className="command">ls ./portfolio</span>
+          {/* Terminal Body */}
+          <div className="terminal-body">
+            <div className="prompt">
+              ~/ryan-dong/site: <span className="command">ls ./portfolio</span>
+            </div>
           </div>
         </div>
-      </div>
-    </Draggable>
+        
+        </Draggable>
+    </ResizableBox>
+    
   );
 };
 
