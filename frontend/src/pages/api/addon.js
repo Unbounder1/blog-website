d9bfaf0e7b69b867136d42549107508a4fa1f307
Ryan Dong <78339287+Unbounder1@users.noreply.github.com>
2025-03-19 16:30:41 -0400

    add components for submitting files

--- frontend/src/pages/api/addon.js
+++ frontend/src/pages/api/addon.js
@@ -9,8 +9,8 @@ export async function GET({ request }) {
     // PATH
     // path_ip = ip host, ie localhost
     // path_port = ip port, ie port
-    // path_specs = 
-    // Fetch Format ->  http://path_ip:path_port/path_specs/
+    // path_spec = 
+    // Fetch Format ->  http://path_ip:path_port/path_spec/
 
     // CONTENT
     // body_content
