2effe161ea1e316d4a21055d156e3c4c6536abc2
Ryan Dong <78339287+Unbounder1@users.noreply.github.com>
2025-04-15 17:57:44 -0400

    Create slider component for parameter controlling

--- frontend/src/components/SliderComponent.jsx
+++ frontend/src/components/SliderComponent.jsx
@@ -1,7 +1,27 @@
-import "../styles/slidercomponent.css";
+import React from 'react';
+import Slider from '@mui/material/Slider';
+import Typography from '@mui/material/Typography';
 
-const SliderComponent = ( {isOn, setIsOn, onString, offString} ) => {
-  
+// Slider component with default min 0, max 100
+// input states [value, setValue] PARAM
+const SliderComponent = ({ value, setValue, title="Parameter Name", minVal=0, maxVal=100 }) => { 
+  const handleChange = (event, newValue) => {
+    setValue(newValue); 
+  };
+
+  return (
+    <div style={{ width: 300, padding: 20 }}>
+      <Typography variant="h6">{title}</Typography>
+      <Slider
+        value={value}
+        onChange={handleChange}
+        min={minVal}
+        max={maxVal}
+        step={1}
+        valueLabelDisplay="auto"
+      />
+    </div>
+  );
 };
 
 export default SliderComponent;
